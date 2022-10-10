91690d170439b5c57fa75a665f9f87ab52a2df9f
Kruceo <rafa2015krueger@gmail.com>
2022-10-10 17:43:41 -0300

    fix: saving timeout

--- cirilo.js
+++ cirilo.js
@@ -1,21 +1,25 @@
+const { exec } = require('child_process');
 const fs = require('fs');
 const { setToVersion } = require('./managePkg');
-
-fs.watchFile('.git/logs/HEAD',()=>{
+let isSaving = false;
+fs.watchFile('.git/index',()=>{
+    if(isSaving)return
 let lines = fs.readFileSync('.git/logs/HEAD', 'utf-8').split('\n');
 let major = 0,minor = 0,patch = 0;
 let commits = lines.filter((line)=>{
     if(line.includes('commit'))
     {
         return line;
+
     }
 })
 
 commits.forEach((commit,index)=>
 {
+    
     let i = commit.split('commit')[1].split(':')[1].trim();
     commits[index] = i;
-
+    
     if(i == 'fix' || i =='init')
     {
         patch ++;
@@ -31,12 +35,15 @@ commits.forEach((commit,index)=>
         minor = 0;
         patch = 0;
     }
-
+    
 
 })
-console.log(commits);
-console.log(major,minor,patch);
 setToVersion(major,minor,patch);
+isSaving = true;
+exec('git add package.json');
+exec('git commit -m')
+setTimeout(()=>isSaving = false,2000)
+
 })
 
 
