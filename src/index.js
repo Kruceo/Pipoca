b289a53fdf7fa7e0c2c904be86a699c0c7b76ee0
Kruceo <rafa2015krueger@gmail.com>
2022-10-11 12:52:44 -0300

    att: use git log --all instead logs/HEAD

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
-const { exec } = require("child_process");
+const { exec, execSync } = require("child_process");
 const fs = require("fs");
 const { coloral: c } = require("coloral");
 const { setToVersion } = require("./lib/managePkg.js");
-const { argv } = require("process");
+const { argv, stdout, stderr } = require("process");
 let isSaving = false;
 
 if (argv.at(2) == "--test") {
   doRead();
+  return;
 }
 
 if (!fs.existsSync(".git"))
@@ -22,62 +23,70 @@ try {
 }
 
 function doRead() {
-  if (isSaving) {
-    console.log(c.markblack("".padEnd(40,' ')));
-    isSaving = false;
-    return;
-  }
-  let lines = fs.readFileSync(".git/logs/HEAD", "utf-8").split("\n");
-  let major = 0,
-    minor = 0,
-    patch = 0;
-  let commits = lines.filter((line) => {
-    if (line.includes("commit")) {
-      return line;
+  exec("git log --all --oneline", (err, stdout, stderr) => {
+    fs.writeFileSync('test',stdout)
+    if (isSaving) {
+      console.log("\n");
+      isSaving = false;
+      return;
     }
-  });
+    let lines = stdout.split("\n");
+    let major = 0,
+      minor = 0,
+      patch = 0;
+    let commits = [];
 
-  commits.forEach((commit, index) => {
-    let i = commit.split("commit")[1].split(":");
-    commits[index] = i;
-    if (i[0].trim() != "(amend)") {
-      if (i[1].trim() == "fix") {
-        patch++;
-      }
-      if (i[1].trim() == "att") {
-        minor++;
-        patch = 0;
-      }
-      if (i[1].trim() == "new") {
-        major++;
-        minor = 0;
-        patch = 0;
-      }
+    lines.forEach((line,index)=>
+    {
+      commits[index] = line.slice(8,line.length);
+     // console.log(line.slice(8,line.length+1));
+    })
+
+    commits.forEach((commit, index) => {
+      let i = commit.split(":")[0];
+      commits[index] = i;
+        if (i.trim() == "fix") {
+          patch++;
+        }
+        if (i.trim() == "att") {
+          minor++;
+          patch = 0;
+        }
+        if (i.trim() == "new") {
+          major++;
+          minor = 0;
+          patch = 0;
+        }
+      
+    });
+    console.log(c.markocean(" INF. ") + " Commits & Amends: " + commits.length);
+    if (commits[commits.length - 1] == "pipoca") {
+      return;
     }
-  });
-  console.log(c.markocean(" INF. ") + " Commits & Amends: " + commits.length);
-  if (commits[commits.length - 1] == "pipoca") {
-    return;
-  }
 
-  let setVersion = setToVersion(major, minor, patch);
-  if (setVersion.error) {
-    console.log(c.markred(" ERR. ") + " replacing version");
-  } else {
-    console.log(c.markgreen(" PKG. ") + " " + setVersion.message);
-  }
-  isSaving = true;
-  exec("git add package.json");
-  let commitProc = exec("git commit --amend --no-edit");
-  commitProc.stdout.on("data", (data) => {
-    console.log(c.markocean(" INF. ") + " Pipoca commited!");
-  });
-  commitProc.stdout.on("error", (data) => {
-    console.log(c.markred(" ERR. ") + " Pipoca commit fails");
-    console.log(data);
-  });
-  commitProc.addListener("exit", (data) => {
-    console.log(c.markocean(" INF. ") + " Commit process closed");
+    let setVersion = setToVersion(major, minor, patch);
+    if (setVersion.error) {
+      console.log(c.markred(" ERR. ") + " replacing version");
+    } else {
+      console.log(c.markgreen(" PKG. ") + " " + setVersion.message);
+    }
+    isSaving = true;
+    if (argv.at(2) == "--test") {
+      return;
+    }
+    exec("git add package.json");
+    let commitProc = exec("git commit --amend --no-edit");
+
+    commitProc.stdout.on("data", (data) => {
+      console.log(c.markocean(" INF. ") + " Pipoca commited!");
+    });
+    commitProc.stdout.on("error", (data) => {
+      console.log(c.markred(" ERR. ") + " Pipoca commit fails");
+      console.log(data);
+    });
+    commitProc.addListener("exit", (data) => {
+      console.log(c.markocean(" INF. ") + " Commit process closed");
+    });
   });
 }
 //setTimeout(() => {isSaving = false;console.log('termited');}, 10000);
