a631baedef464c295212e4b6be9bc8dffce35740
kruceo <kruceo.com@gmail.com>
2024-01-31 17:06:15 -0300

    fix: added support for multiple parent git folder detection

--- src/index.mjs
+++ src/index.mjs
@@ -3,59 +3,68 @@ import fs from 'fs'
 import { history, versionHandler } from "./versionizer.mjs";
 import { createConfigFile, getConfig } from "./config.mjs";
 import * as logger from './logger.mjs'
-const dotGitHead = path.resolve('.git/logs/HEAD')
+
 const config = getConfig()
-function run(){
-    if (!fs.existsSync(dotGitHead)){
+function run() {
+    let parentFolder = './'
+    let dotGitHead = '.git/logs/HEAD'
+    let dotGitfullpath = ''
+    for (let i = 0; i < 5; i++) {
+        dotGitfullpath = path.resolve(parentFolder, dotGitHead)
+        if (!fs.existsSync(dotGitfullpath)) parentFolder = path.resolve(parentFolder, '../')
+    }
+    console.log(parentFolder)
+    // console.log('using ' + dotGitfullpath)
+
+    if (!fs.existsSync(dotGitfullpath)) {
         logger.err('Git Head not exist')
         return
     };
-    if (!fs.existsSync('./package.json')){
+    if (!fs.existsSync('./package.json')) {
         logger.err('package.json not exist')
         return
     };
 
-    if(checkArgv('--create-config','-c')){
+    if (checkArgv('--create-config', '-c')) {
         createConfigFile()
         logger.ok('created config file!')
         process.exit()
     }
-    if(checkArgv('--history','-H')){
+    if (checkArgv('--history', '-H')) {
         history()
         process.exit()
     }
 
-    if(checkArgv('--single-run','-s')){
+    if (checkArgv('--single-run', '-s')) {
         versionHandler()
         process.exit()
     }
 
-    if(checkArgv('--help')){
-        console.log("[Command]".padEnd(30,' ') + ' [Description]')
-        console.log("--create-config -c ".padEnd(30,'.') + " Create a config file with default values.")
-        console.log("--show-history -H ".padEnd(30,'.') + " Show your versions history.")
-        console.log("--single-run -s ".padEnd(30,'.') + " Don't watch, just run one time.")
+    if (checkArgv('--help')) {
+        console.log("[Command]".padEnd(30, ' ') + ' [Description]')
+        console.log("--create-config -c ".padEnd(30, '.') + " Create a config file with default values.")
+        console.log("--show-history -H ".padEnd(30, '.') + " Show your versions history.")
+        console.log("--single-run -s ".padEnd(30, '.') + " Don't watch, just run one time.")
         process.exit()
     }
 
-   logger.info('watcher initiated...')
-    const watcher = fs.watch(dotGitHead,(data)=>{
-       
+    logger.info('watcher initiated...')
+    const watcher = fs.watch(dotGitfullpath, (data) => {
         versionHandler()
     })
 }
 
-export {run}
+export { run }
 // console.log('terminated')
 
-function checkArgv(...args){
+function checkArgv(...args) {
     let include = false
-    args.forEach(each=>{
-        if(include)return;
-        if(process.argv.includes(each)){
+    args.forEach(each => {
+        if (include) return;
+        if (process.argv.includes(each)) {
             include = true
         }
     })
     return include
-  
+
 }
